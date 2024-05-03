98d9d3c2a38f02f48c3dd7331cfa13a3cb6dd9c5
Cezary Ciślak <s25429@pjwstk.edu.pl>
2024-05-03 22:27:04 +0200

    KT-7 added firebase config and dotenv support

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { app as firebaseApp } from './config/firebase'
 
 
 function App() {
+    console.log(firebaseApp)
+
     return (<>
         <h1>Kana Typer Test</h1>
     </>)

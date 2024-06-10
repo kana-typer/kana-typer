2d8fa1f7b65c59aba498a5de27ab2f61dcd1f45e
Cezary Ciślak <s25429@pjwstk.edu.pl>
2024-06-10 18:23:57 +0200

    KT-62 cleanup and added more documentation

--- src/utils/kana.js
+++ src/utils/kana.js
@@ -1,16 +1,24 @@
 import kanaUnicodes from '../data/unicode-map.json'
 
 
-const hiraganaMap = kanaUnicodes.hiragana.array.reduce((acc, kana, index) => (
-  { ...acc, [(parseInt(kanaUnicodes.hiragana.count.start, 16) + index).toString(16)]: kana }
-), {})
+const LONGEST_MORAE_COMBINATION = 10
+const hiraganaMap = parseUnicodeMap(kanaUnicodes)
 
 
-export const unicodeToString = (hex) => String.fromCodePoint(parseInt(hex, 16))
+export const unicodeToKana = (hex) => String.fromCodePoint(parseInt(hex, 16)) // '304b' -> 'か'
 
-export const stringToUnicode = (char) => char.codePointAt(0)?.toString(16) || ''
+export const unicodeToRomaji = (hex) => hiraganaMap?.[hex] || '' // '304b' -> 'ka'
+
+export const kanaToUnicode = (char) => char.codePointAt(0)?.toString(16) || '25a1' // 'か' -> '304b'
+
+export const kanaToRomaji = (char) => unicodeToRomaji(kanaToUnicode(char)) // 'か' -> 'ka'
 
 
+/**
+ * Returns array with valid romaji combinations for given array of morae.
+ * @param {Array<string>} moraeUnicodeArray - e.g. ['3063', '3066'] // って
+ * @returns array with possible inputs using romaji
+ */
 export function getInputCombinations(moraeUnicodeArray) {
   const moraeArray = moraeUnicodeArray.map(code => hiraganaMap[code])
   const combinations = [moraeArray.join('')]
@@ -46,8 +54,14 @@ export function getInputCombinations(moraeUnicodeArray) {
   return combinations
 }
 
+/**
+ * Checks given romaji if its correct, incorrect or unfinished based on array of possible combinations.
+ * @param {string} romaji - e.g. cha
+ * @param {Array<string>} combinations - possible input combinations of morae, e.g. ['cha', 'chixya']
+ * @returns `true` if correct, `false` if incorrect, `undefined` if given romaji is not a valid combination of morae
+ */
 export function checkRomaji(romaji, combinations) {
-  if (romaji.length > 10) // possible longest string one morae combination can create when written in romaji
+  if (romaji.length > LONGEST_MORAE_COMBINATION) // possible longest string one morae combination can create when written in romaji
     return false
 
   if ('aiueo'.split('').some(mora => romaji.endsWith(mora))) // romaji ends on vowel - may be valid morae
@@ -59,10 +73,16 @@ export function checkRomaji(romaji, combinations) {
   return undefined // not finished typing
 }
 
+// TODO: change return type from object to Map: https://www.w3schools.com/js/js_maps.asp
 /**
- * 
- * @param {string} romaji - hiragana representation in romaji
+ * Parses an array of romaji and unicode chars tied to it, creating a map. 
+ * @param {*} map - unicode map object from db
+ * @returns {{ [unicode: string]: string }} map object with unicode as keys and romaji representation as values
  */
-export function romajiToHiragana(romaji) {
-  console.log(hiraganaMap)
+export function parseUnicodeMap(map) {
+  return map.hiragana.array.reduce((acc, kana, index) => (
+    map.hiragana.except.includes(kana)
+      ? acc
+      : { ...acc, [(parseInt(kanaUnicodes.hiragana.count.start, 16) + index).toString(16)]: kana }
+  ), {})
 }
--- src/utils/tests/kana.test.js
+++ src/utils/tests/kana.test.js
@@ -5,3 +5,47 @@ import { unicodeToString } from '../kana'
 test('0x304b === ka', () => {
   expect(unicodeToString('304b')).toBe('か')
 })
+
+// useEffect(() => {
+  // romajiToHiragana('kattechannyaji')
+  // getInputCombinations('ka', visibleKana[checkingIndex])
+
+  // chijimeru -> ちぢめる
+  // tsuzuku -> つづく
+
+  // checkRomaji('k', getInputCombinations([stringToUnicode('か')]))
+  // checkRomaji('ka', getInputCombinations([stringToUnicode('か')]))
+  // checkRomaji('n', getInputCombinations([stringToUnicode('ん')]))
+  // checkRomaji('n', getInputCombinations([stringToUnicode('に')]))
+  // checkRomaji('ni', getInputCombinations([stringToUnicode('に')]))
+  // checkRomaji('n', getInputCombinations([stringToUnicode('ね')]))
+  // checkRomaji('ne', getInputCombinations([stringToUnicode('ね')]))
+  // checkRomaji('t', getInputCombinations([stringToUnicode('つ')]))
+  // checkRomaji('ts', getInputCombinations([stringToUnicode('つ')]))
+  // checkRomaji('tsu', getInputCombinations([stringToUnicode('つ')]))
+  // checkRomaji('j', getInputCombinations([stringToUnicode('じ')]))
+  // checkRomaji('ji', getInputCombinations([stringToUnicode('じ')]))
+  // checkRomaji('k', getInputCombinations([stringToUnicode('か')]))
+  // checkRomaji('e', getInputCombinations([stringToUnicode('か')]))
+  // checkRomaji('r', getInputCombinations([stringToUnicode('り')]))
+  // checkRomaji('rn', getInputCombinations([stringToUnicode('り')]))
+  // checkRomaji('rno', getInputCombinations([stringToUnicode('り')]))
+  // checkRomaji('q', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qv', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvo', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvow', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvowa', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvowad', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvowadi', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvowadis', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvowadiso', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvowadisor', getInputCombinations([stringToUnicode('て')]))
+  // checkRomaji('qvowadisort', getInputCombinations([stringToUnicode('て')]))
+
+  // checkRomaji('ka', getInputCombinations(['3061', '3083']))
+  // checkRomaji('ka', getInputCombinations(['306b', '3083']))
+  // checkRomaji('ka', getInputCombinations(['3063', '3066']))
+  // checkRomaji('ka', getInputCombinations(['3063', '3061']))
+  // checkRomaji('ka', getInputCombinations(['3063', '3061', '3083']))
+  // checkRomaji('ka', getInputCombinations(['3063', '306b', '3083']))
+// }, [])

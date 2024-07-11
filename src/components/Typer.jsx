cf18007cc3100a9eb9370d6925f6a8d49af591e7
Cezary Ciślak <s25429@pjwstk.edu.pl>
2024-07-11 16:22:18 +0200

    KT-68 cleaned up old code and comments

--- src/components/Typer.jsx
+++ src/components/Typer.jsx
@@ -5,57 +5,18 @@ import { getTextWidth } from '../utils/text'
 
 
 function Typer() {
-  const [kanaDictionaryRaw, setKanaDictionaryRaw] = useState({
-    morae: {
-      hiragana: {
-        count: {
-          start: '3041',
-          end: '3093',
-          length: 83
-        },
-        array: [
-          'xa',   'a',              'xi', 'i',                'xu',     'u',                'xe', 'e',              'xo',   'o',
-                  'ka', 'ga',             'ki',   'gi',                 'ku',   'gu',             'ke', 'ge',               'ko', 'go',
-                  'sa', 'za',             'shi',  'ji',                 'su',   'zu',             'se', 'ze',               'so', 'zo',
-                  'ta', 'da',             'chi',  'ji',       'xtsu',   'tsu',  'zu',             'te', 'de',               'to', 'do',
-                  'na',                   'ni',                         'nu',                     'ne',                     'no', 
-                  'ha', 'ba', 'pa',       'hi',   'bi', 'pi',           'fu',   'bu', 'pu',       'he', 'be', 'pe',         'ho', 'bo', 'po',
-                  'ma',                   'mi',                         'mu',                     'me',                     'mo', 
-          'xya',  'ya',                                       'xyu',    'yu',                                       'xyo',  'yo',
-                  'ra',                   'ri',                         'ru',                     're',                     'ro',
-          'xwa',  'wa',                   'wi',                                                   'we',                     'wo',
-          'n'
-        ],
-        except: ['xa', 'xi', 'xu', 'xe', 'xo', 'xwa', 'wi', 'we'],
-      },
-      katakana: {},
-    },
-    words: {
-      hiragana: {},
-      katakan: {},
-      kanji: {},
-    }
-  })
-
   const [kanaOffset, setKanaOffset] = useState(0)
   const [kanaIndex, setKanaIndex] = useState(0)
-  // const [visibleKana, setVisibleKana] = useState([
-  //   ['304b'],         // か
-  //   ['3063', '3066'], // って
-  //   ['3061', '3083'], // ちゃ
-  //   ['3093'],         // ん
-  //   ['306b', '3083'], // にゃ
-  //   ['3058'],         // じ
-  // ])
+
   const [visibleKana, setVisibleKana] = useState(getRandomKana(7))
+  const [userRomaji, setUserRomaji] = useState('')
+
   const [score, setScore] = useState({
     accuracy: 0,
     kana: []
   })
 
   const romajiCombinations = useMemo(() => getInputCombinations(visibleKana[kanaIndex]), [kanaIndex])
-
-  const [userRomaji, setUserRomaji] = useState('')
 
   const checkUserRomaji = (e) => {
     // TODO: could use async/await to first make sure that what user typed is actually rendered and then for next render check if it was even correct

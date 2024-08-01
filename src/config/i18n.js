9b3317720eb86984fb2ba867756571c561039a3f
Cezary Ciślak <s25429@pjwstk.edu.pl>
2024-08-01 18:19:25 +0200

    KT-85 added locales logging for development

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -4,6 +4,8 @@ import { initReactI18next } from 'react-i18next'
 i18next
   .use(initReactI18next)
   .init({
+    // https://www.i18next.com/overview/configuration-options#logging
+    debug: process.env.NODE_ENV === 'development',
     lng: 'en',
     fallback: 'en',
     interpolation: {

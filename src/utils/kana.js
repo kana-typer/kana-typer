2eff7086d216a75d423d6378075a4dca33c99cd5
Cezary Ciślak <s25429@pjwstk.edu.pl>
2024-08-23 17:43:08 +0200

    KT-92 added docstrings, changed some naming and added more safety to code

--- src/utils/kana.js
+++ src/utils/kana.js
@@ -3,13 +3,11 @@ import { createSeededLCGRand } from "./rand"
 export const LONGEST_LETTER_COUNT_PER_MORAE_ALLOWED = 10
 
 /**
- * @param {*} moraArr - mora collection from db, e.g. [{ symbol: 'か', ... }, ...]
+ * Generates an object of small symbols for specific kana modifications in specific scripts, e.g. turning `'か'` to `'っか'`.
+ * @param {Array<{ furigana?: { romaji?: string, hiragana?: string }, script?: string, small?: string }>} moraObj - an array of objects, in which any of the listed properties can be null, but will lead to incomplete data, rather than an error.
+ * @returns {{ sokuon: { hiragana: string | null, katakana: string | null }, yoon: { hiragana: { ya: string | null, yu: string | null, yo: string | null }, katakana: { ya: string | null, yu: string | null, yo: string | null } } }} null in any of the slots if no proper symbol was found
  */
-// export const generateFullMoraMap = (moraArr) => {
-//   return new Map(moraArr.map(({ symbol, ...rest }) => [symbol, rest]))
-// }
-
-export const generateMoraModifiers = (moraMap) => { // TODO: not a map, an object - change it
+export const generateMoraModifiers = (moraObj) => {
   const modifiers = {
     sokuon: { 
       hiragana: null, 
@@ -21,7 +19,7 @@ export const generateMoraModifiers = (moraMap) => { // TODO: not a map, an objec
     },
   }
 
-  moraMap.forEach(mora => {
+  moraObj.forEach(mora => {
     const romaji = mora?.furigana?.romaji 
     if (romaji === 'tsu') 
       modifiers.sokuon[mora?.script] = mora?.small
@@ -32,7 +30,13 @@ export const generateMoraModifiers = (moraMap) => { // TODO: not a map, an objec
   return modifiers
 }
 
-const pickFurigana = (furigana, progressLevel) => { // TODO: furigana is object, add javadocs
+/**
+ * Picks proper furigana if one exists, otherwise returns no furigana.
+ * @param {{ romaji?: string, hiragana?: string }} furigana - object comprised of possible furigana variations for an undisclosed symbol or word
+ * @param {*} progressLevel - level of progress in symbol or word. Dictates the returned furigana, if one exists
+ * @returns {string} proper furigana based on `progressLevel`, but at high enough level or pick some furigana missing, returns an empty string
+ */
+const pickFurigana = (furigana, progressLevel) => {
   const hasHiraganaAsFurigana = furigana?.hiragana !== undefined
   const quantityOffset = hasHiraganaAsFurigana ? 0 : 5
 
@@ -45,22 +49,31 @@ const pickFurigana = (furigana, progressLevel) => { // TODO: furigana is object,
   return ''
 }
 
+/**
+ * Generated a Map object that comprises every possible combination for kana based on given modifiers and filters.
+ * @param {{ symbol: string, furigana?: { romaji?: string, hiragana?: string }, script?: string, extended?: bool, type?: string, small?: string, sokuon?: string, yoon?: '' | string }} sourceObj - raw object of all basic kana. Extended kana is treated as basic kana as it has unique, non-repeatable rules.
+ * @param {{ [symbol: string]: number }} userProgressLevels - specifies progress level for some symbols, which directs given furigana.
+ * @param {{ sokuon: { hiragana: string | null, katakana: string | null }, yoon: { hiragana: { ya: string | null, yu: string | null, yo: string | null }, katakana: { ya: string | null, yu: string | null, yo: string | null } } }} moraModifiers - object used for creating modifications based on specifications given in `sourceObj`.
+ * @param {{}} includeFilers - filters that specify inclusion of symbols based on their properties, e.g. `{ script: hiragana }` includes only symbols that correspond to hiragana.
+ * @param {{}} excludeFilters - filters that specify exclusion of symbols based on their properties, e.g. `{ script: hiragana }` excludes every symbol that corresponds to hiragana.
+ * @returns {Map<string, Array<{ symbol: string, progressLevel: number, furigana: string }>>} a Map object which keys are romaji representations of kana, and values which are sets of objects that could correspond to given romaji.
+ */
 export const generateFilteredMoraMap = (
-  sourceMap, // TODO: not a map, an object - change it
+  sourceObj,
   userProgressLevels,
   moraModifiers, 
-  includeFilers = {}, 
+  includeFilers = {}, // TODO: implement inclusion and exclusion filters
   excludeFilters = {}
 ) => {
   const yoons = ['ya', 'yu', 'yo']
   const filteredMora = new Map([])
 
-  sourceMap.forEach(({ symbol, ...mora }) => {
+  sourceObj.forEach(({ symbol, ...mora }) => {
     const hasSokuon = mora?.sokuon !== undefined
     const hasYoon = mora?.yoon !== undefined
-    const romaji = mora?.furigana?.romaji
-    const script = mora?.script
-    const furigana = mora?.furigana
+    const romaji = mora?.furigana?.romaji || ''
+    const script = mora?.script || 'hiragana'
+    const furigana = mora?.furigana || ''
 
     const items = [
       { 
@@ -85,7 +98,7 @@ export const generateFilteredMoraMap = (
       items.push({
         key: sokuonRomaji, 
         symbol: sokuonSymbol,
-        progressLevel: userProgressLevels?.[symbol] || 0, // TODO: ka or kka? To be specified still so replace it later if needed
+        progressLevel: userProgressLevels?.[symbol] || 0, // TODO: ka or kka for kka? To be specified still so replace it later if needed
         furigana: pickFurigana(sokuonFurigana, userProgressLevels?.[symbol]),
       })
     }
@@ -145,11 +158,12 @@ export const generateFilteredMoraMap = (
 }
 
 /**
- * @param {Map<string, string[]>} moraMap - map of mora to choose from, where key is romaji and value is array of morae
- * @param {{ amount: number, countingSpecificity: 'mora'|'morae', seed: number }} - specify an amount of mora/morae to generate and how counting will be done: mora - counts each individual symbol (e.g. sha = 2), morae - counts every individual sound (e.g. sha = 1)
+ * @param {Map<string, Array<{ symbol: string, furigana: string }>>} moraMap - map of mora to choose from, where key is romaji and value is array of morae.
+ * @param {{ amount: number, countingSpecificity: 'mora'|'morae', seed: number }} options - specify an amount of mora/morae to generate and how counting will be done: mora - counts each individual symbol (e.g. sha = 2), morae - counts every individual sound (e.g. sha = 1)
+ * @returns {Array<{ symbol: string, furigana: string }>}
  */
-export const getRandomMora = (moraMap, { amount, countingSpecificity = 'mora', seed }) => {
-  const rand = createSeededLCGRand(seed ?? '12345')
+export const getRandomMora = (moraMap, { amount, countingSpecificity = 'mora', seed = '12345' }) => {
+  const rand = createSeededLCGRand(seed)
 
   const chosen = []
   const choices = Array
@@ -180,6 +194,13 @@ export const getRandomMora = (moraMap, { amount, countingSpecificity = 'mora', s
   return chosen
 }
 
+/**
+ * Checks given `romaji` against given `target` based on source data from given `map`.
+ * @param {string} romaji - e.g. `'ka'`.
+ * @param {string} target - e.g. `'か'`.
+ * @param {Map<string, Array<{ symbol: string }>>} map - source map that has a lookup for romaji and kana they might correspond to.
+ * @returns `true` if it's valid romaji for given kana, `false` if it is not and `undefined` if given romaji might not have been typed fully, i.e. is not a valid romaji to make a valid japanese sound.
+ */
 export const checkRomajiValidity = (romaji, target, map) => {
   // possible longest string one morae combination can create when written in romaji
   if (romaji.length > LONGEST_LETTER_COUNT_PER_MORAE_ALLOWED)

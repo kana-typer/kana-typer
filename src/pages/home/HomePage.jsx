5f2dc1d88d1019cbd2a70c8174f53a07b3bc0623
s24301 <s24301@pjwstk.edu.pl>
2024-12-07 16:40:46 +0100

    KT-124 html for HomePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,8 +1,19 @@
 function HomePage() {
   return (
-    <div>
-      <h1>hello!</h1>
-    </div>
+    <>
+      <section className="info">
+        <h1>Hello!</h1>
+
+        <h3>About us</h3>
+
+        <h3>What's new?</h3>
+
+      </section>
+      
+      <section className="logo">
+        <img src="#" alt="logo" width="500" height="500"></img>
+      </section>
+    </>
   )
 }
 

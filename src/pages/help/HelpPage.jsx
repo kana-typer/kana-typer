5d4d0969d842805a91ff5cc7dd3000c08aa1cd51
s24301 <s24301@pjwstk.edu.pl>
2024-12-07 16:11:20 +0100

    KT-125 html for HelpPage

--- src/pages/help/HelpPage.jsx
+++ src/pages/help/HelpPage.jsx
@@ -1,8 +1,20 @@
 function HelpPage() {
   return (
-    <div>
-      <h1>basics, help & info</h1>
-    </div>
+    <>
+      <section className="box-top">
+        <h1>Contact Us</h1>
+        <h3>Fill the form...</h3>
+      </section>
+
+      <section className="box-bottom">
+        <h1>Write whatever you like</h1>
+        <form action="#">
+          <input type="text" placeholder="Name"/>
+          <input type="text" placeholder="Message"/>
+          <input type="submit" value="Send"/>
+        </form>
+      </section>
+    </>
   )
 }
 

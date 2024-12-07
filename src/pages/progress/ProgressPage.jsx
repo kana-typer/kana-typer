40345271c72cc0d0bfb638b2eeafd580b48f8f54
s24301 <s24301@pjwstk.edu.pl>
2024-12-07 15:57:18 +0100

    KT-123 made html layout and created css file

--- src/pages/progress/ProgressPage.jsx
+++ src/pages/progress/ProgressPage.jsx
@@ -1,8 +1,50 @@
+import '../css/ProgressPage.css'
+
 function ProgressPage() {
   return (
-    <div>
-      <h1>progression of learning</h1>
-    </div>
+    <>
+      <h1>Progress</h1>
+
+      <form action="#">
+        <input type="text" placeholder="Search" name="search"/>
+        <button>Search</button>
+      </form>
+
+      <section className="progress">
+        <ul>
+          <li>
+            <h3>Some_Word</h3>
+            <div className="progress-bar">
+              <div className="bar"></div>
+            </div>
+          </li>
+          <li>
+            <h3>Some_Word</h3>
+            <div className="progress-bar">
+              <div className="bar"></div>
+            </div>
+          </li>
+          <li>
+            <h3>Some_Word</h3>
+            <div className="progress-bar">
+              <div className="bar"></div>
+            </div>
+          </li>
+          <li>
+            <h3>Some_Word</h3>
+            <div className="progress-bar">
+              <div className="bar"></div>
+            </div>
+          </li>
+          <li>
+            <h3>Some_Word</h3>
+            <div className="progress-bar">
+              <div className="bar"></div>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </>
   )
 }
 

5679c18cc34a93f533dc64dacf4736b68bcaa3a4
=Weronika Szydlik <=weronika.szydlikk@wp.pl>
2024-12-21 19:49:18 +0100

    KT-123 base version of progress page

--- src/pages/progress/ProgressPage.jsx
+++ src/pages/progress/ProgressPage.jsx
@@ -1,49 +1,54 @@
 import '../css/ProgressPage.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function ProgressPage() {
   return (
     <>
-      <h1>Progress</h1>
+      <h1 className='progress-page__title'>Progress</h1>
 
-      <form action="#">
-        <input type="text" placeholder="Search" name="search"/>
-        <button>Search</button>
-      </form>
+      <div className='progress-page__container'>
 
-      <section className="progress">
-        <ul>
-          <li>
-            <h3>Some_Word</h3>
-            <div className="progress-bar">
-              <div className="bar"></div>
-            </div>
-          </li>
-          <li>
-            <h3>Some_Word</h3>
-            <div className="progress-bar">
-              <div className="bar"></div>
-            </div>
-          </li>
-          <li>
-            <h3>Some_Word</h3>
-            <div className="progress-bar">
-              <div className="bar"></div>
-            </div>
-          </li>
-          <li>
-            <h3>Some_Word</h3>
-            <div className="progress-bar">
-              <div className="bar"></div>
-            </div>
-          </li>
-          <li>
-            <h3>Some_Word</h3>
-            <div className="progress-bar">
-              <div className="bar"></div>
-            </div>
-          </li>
-        </ul>
-      </section>
+        <form className='progress-page__form' action='#'>
+          <input className='progress-page__search-input' type='text' placeholder='Search' name='search'/>
+          <button className='progress-page__search-button'><i class="fa-solid fa-magnifying-glass"></i> Search</button>
+        </form>
+
+        <section className='progress'>
+          <ul>
+            <li>
+              <h3>兎</h3>
+              <div className='progress-bar'>
+                <div className='bar'></div>
+              </div>
+            </li>
+            <li>
+              <h3>私</h3>
+              <div className='progress-bar'>
+                <div className='bar'></div>
+              </div>
+            </li>
+            <li>
+              <h3>する</h3>
+              <div className='progress-bar'>
+                <div className='bar'></div>
+              </div>
+            </li>
+            <li>
+              <h3>か</h3>
+              <div className='progress-bar'>
+                <div className='bar'></div>
+              </div>
+            </li>
+            <li>
+              <h3>時々</h3>
+              <div className='progress-bar'>
+                <div className='bar'></div>
+              </div>
+            </li>
+          </ul>
+        </section>
+
+      </div>
     </>
   )
 }

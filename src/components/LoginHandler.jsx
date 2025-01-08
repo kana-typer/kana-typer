3820767450ac02618b627cf4f5e2b091525cd0ac
s24301 <s24301@pjwstk.edu.pl>
2025-01-08 17:44:16 +0100

    KT-144 basic frontend for main nav

--- src/components/LoginHandler.jsx
+++ src/components/LoginHandler.jsx
@@ -1,19 +1,21 @@
 import { emitLoginGoogle, emitLogout, useAuth } from '../context/AuthContext'
 
+import '../css/Nav.css'
+
 function LoginHandler() {
   const { currentUser, currentUserType, isSigningIn } = useAuth()
 
   return isSigningIn ? (
-    <span>loading...</span>
+    <span className='login-handler__loading-text'>loading...</span>
   ) : (
     currentUserType === 'google' ? (
       <>
-        <span style={{ marginRight: '1rem' }}>{`Logged in as ${currentUser?.displayName || 'unknown'} <${currentUser?.email || 'unknown'}>`}</span>
+        <span className='login-handler__google'>{`Logged in as ${currentUser?.displayName || 'unknown'} <${currentUser?.email || 'unknown'}>`}</span>
         <button onClick={emitLogout}>Log out from Google</button>
       </>
     ) : (
       <>
-        <span style={{ marginRight: '1rem' }}>Logged in as anonymous</span>
+        <span className='login-handler__google'>Logged in as anonymous</span>
         <button onClick={emitLoginGoogle}>Sign in with Google</button>
       </>
     )

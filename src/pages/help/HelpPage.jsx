9b8175499b7713bc1de7efbe42a916c8cc309c53
s24301 <s24301@pjwstk.edu.pl>
2025-01-25 20:45:37 +0100

    KT-175 remake of help page

--- src/pages/help/HelpPage.jsx
+++ src/pages/help/HelpPage.jsx
@@ -18,18 +18,47 @@ function HelpPage() {
   return (
     <>
       <section className="help-page__box-top">
-        <h1 className="help-page__header">{t('helpDetails.contact')}</h1>
+        <h1 className="help-page__header">{t('helpDetails.helpTitle')}</h1>
         <h3 className="help-page__description">{t('helpDetails.helpDesc')}</h3>
       </section>
 
-      <section className="help-page__box-bottom">
-        <h1 className="help-page__title">{t('helpDetails.writeForm')}</h1>
-        <form className="help-page__form" action="#" method="get">
-          <input className="help-page__fname" type="text" placeholder={t('helpDetails.nameForm')}/>
-          <textarea className="help-page__fmessage" type="text" placeholder={t('helpDetails.messageForm')}/>
-          <button className="help-page__fsubmit" type="submit">{t('helpDetails.sendForm')}</button>
-        </form>
-      </section>
+      <div className='help-page__content'>
+
+        <section className="help-page__box-middle">
+          <div className='help-page__link'>
+            <h3 className='help-page__topic'>{t('helpDetails.writeLink')}</h3>
+            <h4 className='help-page__desc'>{t('helpDetails.linkIssue')}</h4>
+            <a className='help-page__issues' href='https://github.com/kana-typer/kana-typer/issues'>{t('helpDetails.helpLink')}</a>
+          </div>
+        </section>
+
+        <section className="help-page__box-bottom">
+          <div className='help-page__dictionary'>
+            <h3 className='help-page__topic'>{t('helpDetails.helpDictionary')}</h3>
+            <ul className='help-page__dictionary__ul'>
+              <li><h4 className='help-page__dictionary__ul-phrase'>{t('helpDetails.helpHira')}</h4> {t('helpDetails.helpHiraDesc')}</li>
+              <li><h4 className='help-page__dictionary__ul-phrase'>{t('helpDetails.helpKata')}</h4> {t('helpDetails.helpKataDesc')}</li>
+              <li><h4 className='help-page__dictionary__ul-phrase'>{t('helpDetails.helpGojuon')}</h4> {t('helpDetails.helpGojuonDesc')}</li>
+              <li><h4 className='help-page__dictionary__ul-phrase'>{t('helpDetails.helpDakuten')}</h4> {t('helpDetails.helpDakutenDesc')}</li>
+              <li><h4 className='help-page__dictionary__ul-phrase'>{t('helpDetails.helpHandakuten')}</h4> {t('helpDetails.helpHandakutenDesc')}</li>
+              <li><h4 className='help-page__dictionary__ul-phrase'>{t('helpDetails.helpKanji')}</h4> {t('helpDetails.helpKanjiDesc')}</li>
+              <li><h4 className='help-page__dictionary__ul-phrase'>{t('helpDetails.helpRomaji')}</h4> {t('helpDetails.helpRomajiDesc')}</li>
+              <li><h4 className='help-page__dictionary__ul-phrase'>{t('helpDetails.helpFurigana')}</h4> {t('helpDetails.helpFuriganaDesc')}</li>
+            </ul>
+          </div>
+          <div className='help-page__tutorial'>
+            <h3 className='help-page__topic'>{t('helpDetails.helpTutorial')}</h3>
+            <ol className='help-page__tutorial__ol'>
+                <li>{t('helpDetails.helpTutorial1')}</li>
+                <li>{t('helpDetails.helpTutorial2')}</li>
+                <li>{t('helpDetails.helpTutorial3')}</li>
+                <li>{t('helpDetails.helpTutorial4')}</li>
+                <li>{t('helpDetails.helpTutorial5')}</li>
+                <li>{t('helpDetails.helpTutorial6')}</li>
+            </ol>
+          </div>
+        </section>
+      </div>
     </>
   )
 }

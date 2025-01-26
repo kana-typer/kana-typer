661de1750f64e8bc7d0854136497318fb9fc3554
Cezary Ciślak <s25429@pjwstk.edu.pl>
2025-01-26 15:47:59 +0100

    KT-176 simplified progress page logic

--- src/pages/progress/ProgressPage.jsx
+++ src/pages/progress/ProgressPage.jsx
@@ -1,33 +1,19 @@
 import '../css/ProgressPage.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
-import { useEffect, useState } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 
 import { useTranslation } from 'react-i18next'
 import { useGoogleAuth } from '../../context/GoogleAuthContext'
-import { getUserField } from '../../utils/db'
 
 function ProgressPage() {
-  const { currentUser } = useGoogleAuth()
+  const { currentUser, userData } = useGoogleAuth()
   const location = useLocation()
 
   if (currentUser === null || currentUser.isAnonymous)
     return <Navigate to='/login' state={{ from: location }} replace />
 
   const { i18n, t } = useTranslation()
-
-  const [progress, setProgress] = useState({})
-
-  useEffect(() => {
-    const fetcher = async () => {
-      const data = await getUserField('progress') || {}
-      console.debug('Progress data fetched from db')
-      setProgress(data)
-    }
-
-    fetcher()
-  }, [])
 
   return (
     <>
@@ -37,7 +23,6 @@ function ProgressPage() {
       </section>
 
       <div className='progress-page__container'>
-
         <form className='progress-page__form' action='#'>
           <input className='progress-page__search-input' type='text' placeholder={t('common.search')} name='search'/>
           <button className='progress-page__search-button'>
@@ -47,7 +32,7 @@ function ProgressPage() {
 
         <section className='progress'>
           <ul>
-            {Object.entries(progress).map(([kana, progNum], idx) => (
+            {Object.entries(userData?.progress || {}).map(([kana, progNum], idx) => (
               <li key={idx}>
                 <h3>{kana}</h3>
                 <div className="progress-bar">
@@ -56,41 +41,7 @@ function ProgressPage() {
               </li>
             ))}
           </ul>
-          {/* code for debugging:
-          <ul>
-            <li>
-              <h3>兎</h3>
-              <div className='progress-bar'>
-                <div className='bar'></div>
-              </div>
-            </li>
-            <li>
-              <h3>私</h3>
-              <div className='progress-bar'>
-                <div className='bar'></div>
-              </div>
-            </li>
-            <li>
-              <h3>する</h3>
-              <div className='progress-bar'>
-                <div className='bar'></div>
-              </div>
-            </li>
-            <li>
-              <h3>か</h3>
-              <div className='progress-bar'>
-                <div className='bar'></div>
-              </div>
-            </li>
-            <li>
-              <h3>時々</h3>
-              <div className='progress-bar'>
-                <div className='bar'></div>
-              </div>
-            </li>
-          </ul> */}
         </section>
-
       </div>
     </>
   )
